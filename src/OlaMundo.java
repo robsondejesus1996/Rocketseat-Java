2c8e979dd07283fbfe9f3d60491edea7230f5a64
Robson de Jesus <robsondejesus1996@hotmail.com>
2023-10-25 09:22:03 -0300

    Tipos de dados

--- src/OlaMundo.java
+++ src/OlaMundo.java
@@ -1,5 +1,17 @@
 public class OlaMundo {
     public static void main(String[] args) {
-        System.out.println("Ola mundo");
+        // Numeros (int, double, float, long)
+        // Textos (String, char)
+        // Booleano (boolean = true or false)
+
+        int dadoDoTipoInt = 10;
+        double dadoDoTipoDouble = 3.14;
+        float dadoDoTipoFloat = 3.14f;
+        long dadoDoTipoLong = 5565465465456465433l;
+        String dadoDoTipoString = "Robson de Jesus";
+        boolean dadoDoTipoBoolean = false;
+
+        System.out.println(dadoDoTipoInt);
+
     }
 }
