39686b276d5a3c1409d77a077bda099317ca6d90
Robson de Jesus <robsondejesus1996@hotmail.com>
2023-11-13 17:26:50 -0300

    atualização

---
+++ src/wrapper/Wrapper.java
@@ -0,0 +1,15 @@
+package wrapper;
+
+public class Wrapper {
+    public static void main(String[] args) {
+        int tipoInt = 1;
+        float tipoFloat = 1.0f;
+        boolean tpoBoolean = false;
+
+        // int - char
+        Float tipoFloat2 = 1.0F;
+        Boolean tipBoolean2 = false;
+        Integer tipoInteger = 1;
+
+    }
+}
